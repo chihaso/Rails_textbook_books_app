c27cfee6828cf250ccf5ac6ad5879c9227b87025
chihaso <cycyccq@gmail.com>
2019-09-24 14:19:37 +0900

    add the func to set locale from TLD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = extract_locale_from_tld || I18n.default_locale
+  end
+
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').first
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
 end
