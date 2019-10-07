8e90b9b0be7f34004b69de143b0d2c195110b1f3
chihaso <cycyccq@gmail.com>
2019-10-07 17:23:45 +0900

    Add user profile page and permit postal_code, street_address, self_introduction.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_locale
     I18n.locale = extract_locale_from_tld || I18n.default_locale
@@ -10,5 +11,15 @@ class ApplicationController < ActionController::Base
   def extract_locale_from_tld
     parsed_locale = request.host.split(".").first
     I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+
+  def after_sign_in_path_for(resources)
+    books_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code, :street_address, :self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :street_address, :self_introduction])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "books#index"
+
+  resources :users, only: [:show]
+
 end
