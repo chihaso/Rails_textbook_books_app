e88bfbd97a814c5f511cc440c8f7e3a819c9bf1c
chihaso <cycyccq@gmail.com>
2019-10-16 13:01:04 +0900

    Remove unnecessary lines.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   get 'users/show'
+  resources :books
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "books#index"
-
   resources :users, only: [:show]
-
 end
