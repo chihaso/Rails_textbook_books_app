b4fc5205cce3e03aeca8cab35d381aef77a91b9f
chihaso <cycyccq@gmail.com>
2019-10-16 17:42:26 +0900

    Remove unnecessary "request.domain".

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def create_language_links(language_code)
     if language_code == "ja"
-      link_to_if params[:locale] != "ja", "日本語",   "#{request.domain.split("/")[2]}#{request.path}?locale=ja"
+      link_to_if params[:locale] != "ja", "日本語",   "#{request.path}?locale=ja"
     elsif language_code == "en"
-      link_to_if params[:locale] != "en", "English", "#{request.domain.split("/")[2]}#{request.path}?locale=en"
+      link_to_if params[:locale] != "en", "English", "#{request.path}?locale=en"
     else
       exit
     end
