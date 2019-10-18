7e4e772c4b810ca09c0480ac4fd5b102f140f2ef
chihaso <cycyccq@gmail.com>
2019-10-18 18:58:38 +0900

    Fix offenses detected by rubocop with autocorrect.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'users/show'
+  get "users/show"
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: "users/registrations"
   }
   root to: "books#index"
   resources :users, only: [:show]
