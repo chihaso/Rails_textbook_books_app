00e79cc45120a52b95e48d9d05807817b9145cba
chihaso <cycyccq@gmail.com>
2019-10-30 14:56:20 +0900

    Add the sample for avatar.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code, :street_address, :self_introduction, :avatar])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :street_address, :self_introduction, :avatar])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:postal_code, :street_address, :self_introduction, :avatar, :remove_avatar])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :street_address, :self_introduction, :avatar, :remove_avatar])
     end
 end
