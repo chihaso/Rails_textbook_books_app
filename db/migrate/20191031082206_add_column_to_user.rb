ac23ce3ec65063d88c781ccd83854b519ce4d6e7
chihaso <cycyccq@gmail.com>
2019-10-31 17:34:25 +0900

    Add name field in User model.

---
+++ db/migrate/20191031082206_add_column_to_user.rb
@@ -0,0 +1,5 @@
+class AddColumnToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :name, :string, default: "No-name-san"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_071630) do
+ActiveRecord::Schema.define(version: 2019_10_31_082206) do
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -65,6 +64,7 @@ ActiveRecord::Schema.define(version: 2019_10_31_071630) do
     t.text "self_introduction"
     t.string "provider"
     t.string "uid"
+    t.string "name", default: "No-name-san"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
