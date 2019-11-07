d993f2effbe779cb48d42b23729324610b9f1f1d
chihaso <cycyccq@gmail.com>
2019-11-07 12:58:35 +0900

    Eliminate page transitions when the follow / unfollow button is pressed.

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,14 +2,20 @@
 
 class FollowsController < ApplicationController
   def create
-    user = User.find(params[:followee_id])
-    current_user.follow(user)
-    redirect_to user_path(user.id)
+    @user = User.find(params[:followee_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to user_path(@user.id) }
+      format.js
+    end
   end
 
   def destroy
-    user = Follow.find(params[:id]).followee
-    current_user.unfollow(user)
-    redirect_to user_path(user.id)
+    @user = Follow.find(params[:id]).followee
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to user_path(@user.id) }
+      format.js
+    end
   end
 end
