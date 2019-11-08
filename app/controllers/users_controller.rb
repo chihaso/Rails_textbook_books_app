aef39fa09d0720069ebab4502c483d22aca08444
chihaso <cycyccq@gmail.com>
2019-11-08 22:30:06 +0900

    Limit access to user posts page to followers only.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :limit_unfollower, { only: [:posts] }
+
   def show
     @user = User.find(params[:id])
   end
@@ -24,5 +26,12 @@ class UsersController < ApplicationController
   def posts
     @user = User.find(params[:id])
     @posts = Kaminari.paginate_array(@user.posts).page(params[:page]).per(5)
+  end
+
+  def limit_unfollower
+    user = User.find(params[:id])
+    unless current_user.following?(user) || current_user == user
+      redirect_back fallback_location: root_path, notice: t("errors.messages.follower_only")
+    end
   end
 end
