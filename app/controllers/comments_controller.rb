e5b2d115c772e0ceb29bc2665b1249bfc2e56efa
chihaso <cycyccq@gmail.com>
2019-11-14 17:48:59 +0900

    Add comment edit and delete func.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :destroy]
+
   def create
     @comment = Comment.new(comment_params)
     @comment.post_user = User.find current_user.id
@@ -17,12 +19,35 @@ class CommentsController < ApplicationController
   end
 
   def update
+    if @comment.update(comment_params)
+      if @comment.book_id
+        redirect_to book_path(@comment.book_id), notice: t(".success.update")
+      elsif @comment.report_id
+        redirect_to report_path(@comment.report_id), notice: t(".success.update")
+      else
+        redirect_to root_path
+      end
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @comment.destroy
+    if @comment.book_id
+      redirect_to book_path(@comment.book_id), notice: t(".success.destroy")
+    elsif @comment.report_id
+      redirect_to report_path(@comment.report_id), notice: t(".success.destroy")
+    else
+      redirect_to root_path
+    end
   end
 
   private
+    def set_comment
+      @comment = Comment.find params[:id]
+    end
+
     def comment_params
       params.require(:comment).permit(:memo, :user_id, :book_id, :report_id)
     end
