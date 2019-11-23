5e7eab3255863c9c60c5a1a2a10c6d5018bb6fef
chihaso <cycyccq@gmail.com>
2019-11-23 14:58:22 +0900

    Modify create_language_links ro receive request path.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def create_language_links(language_code)
+  def create_language_links(language_code, request_path)
     if language_code == "ja"
-      link_to_if params[:locale] != "ja", "日本語",   "#{request.path}?locale=ja"
+      link_to_if params[:locale] != "ja", "日本語",   "#{request_path}?locale=ja"
     elsif language_code == "en"
-      link_to_if params[:locale] != "en", "English", "#{request.path}?locale=en"
+      link_to_if params[:locale] != "en", "English", "#{request_path}?locale=en"
     else
       t("applicationhelper.create_language_links.unsupported_language")
     end
